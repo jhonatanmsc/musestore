5d8f2f94ce0fe134d196aaca9991957c1829f709
jhonatanmsc <jhonmscosta@gmail.com>
2020-10-03 00:28:18 -0300

    added: sub categories endpoint

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 
-from apps.core.views import ProductViewSet
+from apps.core.views import ProductViewSet, CategoryViewSet, SubCategoryViewSet
 from musestore import drf_router
 
 core_router = drf_router
 core_router.register(r'products', ProductViewSet)
+core_router.register(r'categories', CategoryViewSet)
+core_router.register(r'sub-category', SubCategoryViewSet)
 
 urlpatterns = [
     path('', include(core_router.urls))
