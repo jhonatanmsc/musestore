b555343ed0e97960821e06ab90ed95b5a5d90400
jhonatanmsc <jhonmscosta@gmail.com>
2020-10-03 00:26:50 -0300

    added: is sub category field in model category

--- apps/core/models.py
+++ apps/core/models.py
@@ -4,14 +4,15 @@ from django.db import models
 
 
 class Category(models.Model):
-    title = models.CharField(u'título', max_length=120)
+    title = models.CharField(u'título', unique=True, max_length=120)
+    is_sub_category = models.BooleanField(default=True)
 
     class Meta:
         verbose_name = 'Categoria'
         verbose_name_plural = 'Categorias'
 
     def __str__(self):
-        return f'Categoria: {self.title}'
+        return self.title
 
 
 class Product(models.Model):
@@ -19,7 +20,7 @@ class Product(models.Model):
     description = models.TextField(u'descrição', max_length=254, null=True, blank=True)
     price = models.DecimalField(u'preço', max_digits=10, decimal_places=2, default=0.0)
     image = models.ImageField(u'foto', upload_to='product', max_length=254, null=True, blank=True)
-    category = models.ForeignKey(Category, models.CASCADE, related_name='all_products')
+    category = models.ForeignKey(Category, models.CASCADE, null=True, blank=True, related_name='all_products')
     sub_categories = models.ManyToManyField(Category)
     created_at = models.DateTimeField(u'criado em', auto_now=True)
     updated_at = models.DateTimeField(u'atualizado em', auto_now_add=True)
